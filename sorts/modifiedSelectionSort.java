8886170c040af7ae23383c1cdb8d8cd0780fbc6f
sir-grr <grr1206@gmail.com>
2024-04-24 13:43:46 +0100

    removed unneeded comment

--- sorts/modifiedSelectionSort.java
+++ sorts/modifiedSelectionSort.java
@@ -44,7 +44,6 @@ public class modifiedSelectionSort extends sort{
             }
             //printNumList(numList);
 
-            //printNumList(numList);
         }
         return numList;
     }
