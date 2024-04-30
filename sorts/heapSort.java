b562e0ed4865579dc0f5a34defc3eb8eb1102cfb
sir-grr <grr1206@gmail.com>
2024-04-30 17:24:25 +0100

    First Init Heap and Merge sort

---
+++ sorts/heapSort.java
@@ -0,0 +1,10 @@
+package sorts;
+
+import java.util.ArrayList;
+
+public class heapSort extends sort{
+    @Override
+    public ArrayList<Integer> sortArray(ArrayList<Integer> numList){
+        return numList;
+    }
+}
---
+++ sorts/mergeSort.java
@@ -0,0 +1,10 @@
+package sorts;
+
+import java.util.ArrayList;
+
+public class mergeSort extends sort{
+    @Override
+    public ArrayList<Integer> sortArray(ArrayList<Integer> numList){
+        return numList;
+    }
+}
