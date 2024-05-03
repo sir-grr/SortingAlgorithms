b338ce28bf3d1c040e79e223ba98149c260527bc
sir-grr <grr1206@gmail.com>
2024-05-03 15:44:00 +0100

    Updatig sort Test

--- sortTest.java
+++ sortTest.java
@@ -2,64 +2,73 @@
 //this is an where we can run all our sorts from 
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 import sorts.*;
 
 public class sortTest {
     private static ArrayList<ArrayList<Integer>> testInputs = new ArrayList<>();
-    private static ArrayList<ArrayList<Integer>> testExpectedOutputs = new ArrayList<>();
 
     public static void main(String[] args) {
         addData();
         //for each sort run tests
-        sort[] sorts = {new middlePivotQuickSort(), new medianPivotQuickSort(),new randomPivotQuickSort(),/*new randomisedInsertionSort()/*,new stalinSort(),new insertionSort(),new modifiedSelectionSort(), new selectionSort(),/*new bubbleSort(), new improvedBubbleSort()/* , new testSort()*/};
+        sort[] sorts = {new bubbleSort(),new middlePivotQuickSort(), new medianPivotQuickSort(),new randomPivotQuickSort(),/*new randomisedInsertionSort()/*,new stalinSort(),new insertionSort(),new modifiedSelectionSort(), new selectionSort(),/*new bubbleSort(), new improvedBubbleSort()/* , new testSort()*/};
         //printNumList(list);
         for(sort sort : sorts){
-            System.out.println("Testing " + sort.getClass().getName().substring(6));
-            for(int i = 0; i < testInputs.size();i++){
-                //we make a copy to ensure we aren't altering the actual data we need to use
-                ArrayList<Integer> inputCopy = new ArrayList<>(testInputs.get(i));
-                inputCopy = sort.sortArray(inputCopy);
-                //if it equals test data, is success
-                if(inputCopy.equals(testExpectedOutputs.get(i))){
-                    System.out.println("Test " + (i+1) + " passed, data sorted");
-                } else {
-                    System.out.println("Test " + (i+1) + " failed, Expected:");
-                    printNumList(testExpectedOutputs.get(i));
-                    System.out.println("Actually recieved :");
-                    printNumList(inputCopy);
-                }
-            }
+            test(sort);
         }
 
     }
 
+    public static void test(sort sort){
+        System.out.println("Testing " + sort.getClass().getName().substring(6));
+            for(int i = 0; i < testInputs.size();i++){
+                //we make a copy to ensure we aren't altering the actual data we need to use
+                ArrayList<Integer> inputCopy = new ArrayList<>(testInputs.get(i));
+                //we sort the copy
+                inputCopy = sort.sortArray(inputCopy);
+                //if it is sorted we publish a success
+                if(isSorted(inputCopy)){
+                    System.out.println("Test " + (i+1) + " passed, data sorted");
+                } else { // otherwise a failure
+                    System.out.println("Actually recieved :");
+                    printNumList(inputCopy);
+                    System.out.println("Test " + (i+1) + " failed");
+                    Collections.sort(inputCopy);//sort it to show expected value
+                    printNumList(inputCopy);
+                }
+            }
+    }
+
+    public static boolean isSorted(ArrayList<Integer> unCheckedList){
+        ArrayList<Integer> sortedList = new ArrayList<>(unCheckedList);
+        Collections.sort(sortedList);
+        if (unCheckedList.equals(sortedList)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public static void addData(){
         int[] test1Input = {123, 426, 390, 621, 402,231,426,123};
-        int[] test1Output = {123, 123, 231, 390, 402, 426, 426, 621};
-        addDataNest(test1Input, test1Output);
+        addDataNest(test1Input);
 
         int[] test2Input = {1,3,3,2,5};
-        int[] test2Output = {1,2,3,3,5};
-        addDataNest(test2Input, test2Output);
+        addDataNest(test2Input);
 
         int[] test3Input = {1,0,0,3,4};
-        int[] test3Output = {0,0,1,3,4};
-        addDataNest(test3Input, test3Output);
+        addDataNest(test3Input);
 
         int[] test4Input = {1,1,1,1,1,1,1,1,1};
-        int[] test4Output = {1,1,1,1,1,1,1,1,1};
-        addDataNest(test4Input, test4Output);
+        addDataNest(test4Input);
 
         int[] test5Input = {57};
-        int[] test5Output = {57};
-        addDataNest(test5Input, test5Output);
+        addDataNest(test5Input);
 
         int[] test6Input = {};
-        int[] test6Output = {};
-        addDataNest(test6Input, test6Output);
+        addDataNest(test6Input);
 
         Random rng = new Random();
         int randomCount = rng.nextInt(10,150);
@@ -67,23 +76,16 @@ public class sortTest {
         for (int i = 0; i < randomCount;i++){
             test7Input[i] = rng.nextInt(0,1000);
         }
-        int[] test7Output = test7Input.clone();
-        Arrays.sort(test7Output);
-        addDataNest(test7Input, test7Output);
+        addDataNest(test7Input);
     }
 
-    public static void addDataNest(int[] testInput,int[] testOutput){
+    public static void addDataNest(int[] testInput){
         ArrayList<Integer> newInputList = new ArrayList<Integer>();
-        ArrayList<Integer> newOutputList = new ArrayList<Integer>();
         
         for (int num : testInput){
             newInputList.add(num);
         }
-        for (int num : testOutput){
-            newOutputList.add(num);
-        }
         testInputs.add(newInputList);
-        testExpectedOutputs.add(newOutputList);
     }
 
 
