3e1a6ac3d5399bf96ebf5468d47470dbe45119ad
sir-grr <grr1206@gmail.com>
2024-05-12 22:18:24 +0100

    Started merge sort commits

--- sorts/mergeSort.java
+++ sorts/mergeSort.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class mergeSort extends sort{
     @Override
     public ArrayList<Integer> sortArray(ArrayList<Integer> numList){
+        doMergeSort(numList);
         return numList;
+    }
+
+    private void doMergeSort(ArrayList<Integer> numList){
+        System.out.println("in doMergeSort");
     }
 }
