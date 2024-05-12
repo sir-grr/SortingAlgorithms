78dc37786b02b53be11a4d359e90dde784b0292d
sir-grr <grr1206@gmail.com>
2024-05-12 23:00:55 +0100

    Removing code I'm not using

--- sorts/testSort.java
+++ sorts/testSort.java
@@ -1,29 +1,12 @@
 package sorts;
-// bubblesort.java
-//this is an example of bubblesort in Java 
 
 import java.util.ArrayList;
-//import java.util.Random;
 
 public class testSort extends sort {
     //this exists to test out changes to sorts
     @Override
     public ArrayList<Integer> sortArray(ArrayList<Integer> numList){
         System.out.println("in test sort");
-        int lastNum = 0;
-        //loop through list
-        for(int i = 1; i< numList.size(); i++){
-            lastNum = numList.get(i-1);
-            //swap the previous number with this number if it is bigger
-            if (lastNum > numList.get(i)){
-                numList.remove(i-1);
-                numList.add(i, lastNum);
-                //if there is a swap go back to the start of the list
-                i = 0;
-            }
-            printNumList(numList);
-            //System.out.println("end of for");
-        }
         return numList;
     }
 }
