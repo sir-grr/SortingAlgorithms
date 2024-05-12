6eb97defe069e0a7ac622c98ca1ac87e4137c336
sir-grr <grr1206@gmail.com>
2024-05-12 22:18:41 +0100

    Updated bubble sort to use less memory

--- sorts/bubbleSort.java
+++ sorts/bubbleSort.java
@@ -9,15 +9,12 @@ public class bubbleSort extends sort{
     @Override
     public ArrayList<Integer> sortArray(ArrayList<Integer> numList){
         for(int n = 0; n< numList.size(); n++){
-            int lastNum = numList.get(0);
             //loop through list
             for(int i = 1; i< numList.size(); i++){
                 //swap the previous number with this number if it is bigger
-                if (lastNum > numList.get(i)){
+                if (numList.get(i-1) > numList.get(i)){
                     swap(numList, i-1, i);
                 }
-                //System.out.println("end of for");
-                lastNum = numList.get(i);
             }
         }
         return numList;
