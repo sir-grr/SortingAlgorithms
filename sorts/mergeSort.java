e7ebf2ca3691a3fbf652668ba9eeab30ff7a1b3d
sir-grr <grr1206@gmail.com>
2024-05-16 23:36:06 +0100

    Fixing mergeSort

--- sorts/mergeSort.java
+++ sorts/mergeSort.java
@@ -7,28 +7,32 @@ public class mergeSort extends sort{
     @Override
     public ArrayList<Integer> sortArray(ArrayList<Integer> numList){
         if(numList.size()>1){
-            doMergeSort(numList);
+            //we do a lot of passsing back up so we have to at the very end, we convert back to arraylist cause we use the raw list type a lot for sublist here
+            numList = new ArrayList<Integer>(doMergeSort(numList));
         }
         return numList;
     }
 
-    private void doMergeSort(List<Integer> numList){
+    private List<Integer> doMergeSort(List<Integer> numList){
         //get list size
+        //if their is 1 or less value in the list just return it
         int listSize = numList.size();
         if(listSize<=1){
-            return;
+            return numList;
         }
         //get middle of list so we can break it in half
         int midpoint = listSize/2;
-        List<Integer> lList = numList.subList(0, midpoint);//create left partition
-        List<Integer> rList = numList.subList(midpoint, listSize);//create right partition
-        //further divide each
-        doMergeSort(lList);
-        doMergeSort(rList);
-        doMerge(numList, lList, rList);
+        //treat each half as new lists which don't alter the parent
+        List<Integer> lList = new ArrayList<>(numList.subList(0, midpoint));//create left partition
+        List<Integer> rList = new ArrayList<>(numList.subList(midpoint, listSize));//create right partition
+        //further divide each and merge sort them
+        lList = doMergeSort(lList);
+        rList = doMergeSort(rList);
+        //merge back into the parent
+        return doMerge(new ArrayList<>(numList), lList, rList);
     }
 
-    private void doMerge(List<Integer> parentList,List<Integer> lList,List<Integer> rList){
+    private List<Integer> doMerge(List<Integer> parentList,List<Integer> lList,List<Integer> rList){
         int p=0,l=0,r=0;//these are the indices we are using to track through our lists
         //while there are still elements left in each list
         while (l < lList.size() && r < rList.size()) {
@@ -42,6 +46,17 @@ public class mergeSort extends sort{
             }
             p++;
         }
+        //only one of these two will run because one list is run empty
+        //add any remaining in the left list
+        for (;l<lList.size();l++,p++){
+            parentList.set(p, lList.get(l));
+        }
+        //add any remaining in the right list
+        for (;r<rList.size();r++,p++){
+            parentList.set(p, rList.get(r));
+        }
+        //return the sorted parent
+        return parentList;
 
     }
 }
